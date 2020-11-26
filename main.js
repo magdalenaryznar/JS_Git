441572167a891a8108bd8d311ff63411b0ad054b
Bartek-Szewczyk <ba20001000@gmail.com>
2020-11-26 13:23:19 +0100

    js slide incorrect

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const gall = document.querySelectorAll("img");
 console.log(gall);
 const nxt = document.querySelector('#next')
-nxt.addEventListener("click", () => {
+
+nxt.addEventListener("click", next)
+
+function next() {
     for (img in gall) {
         if (gall[img].classList == 'visible') {
-            gall[img].classList.remove("visible")
             const nextt = gall[img].nextElementSibling
+            gall[img].classList.remove("visible")
             console.log(nextt);
             nextt.classList.add('visible')
 
         }
     }
 
-})
+}
