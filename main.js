01d907bb3fe89518e660b55004699afd6035d06a
Bartek-Szewczyk <ba20001000@gmail.com>
2020-12-02 17:38:05 +0100

    load images

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-const img = new Image();
-img.src = 'https://picsum.photos/700/300';
-
-
 let gallery = document.querySelector('.galleryJS')
-gallery.appendChild(img);
+
+const img1 = new Image();
+img1.addEventListener('load', () => {
+    gallery.appendChild(img1);
+    const img2 = new Image();
+    img2.addEventListener('load', () => {
+        gallery.appendChild(img2);
+        const img3 = new Image();
+        img3.addEventListener('load', () => {
+            gallery.appendChild(img3);
+            const img4 = new Image();
+            img4.addEventListener('load', () => {
+                gallery.appendChild(img4);
+                const img5 = new Image();
+                img5.addEventListener('load', () => {
+                    gallery.appendChild(img5)
+                })
+            })
+        })
+    })
+})
