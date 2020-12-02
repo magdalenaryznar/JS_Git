ffea6f4b629e8b8028efe4a8ae10f2a0ccb34cca
Bartek-Szewczyk <ba20001000@gmail.com>
2020-12-02 17:42:58 +0100

    załadowanie wszystkich img

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-let gallery = document.querySelector('.galleryJS')
+let gallery = document.querySelector('.galleryJS');
 
 const img1 = new Image();
+img1.src = 'https://picsum.photos/700/300';
 img1.addEventListener('load', () => {
     gallery.appendChild(img1);
     const img2 = new Image();
+    img2.src = 'https://picsum.photos/700/300';
     img2.addEventListener('load', () => {
         gallery.appendChild(img2);
         const img3 = new Image();
+        img3.src = 'https://picsum.photos/700/300';
         img3.addEventListener('load', () => {
             gallery.appendChild(img3);
             const img4 = new Image();
+            img4.src = 'https://picsum.photos/700/300';
             img4.addEventListener('load', () => {
                 gallery.appendChild(img4);
                 const img5 = new Image();
+                img5.src = 'https://picsum.photos/700/300';
                 img5.addEventListener('load', () => {
                     gallery.appendChild(img5)
+                    console.log('Wszystkie zdjęcia załadowane');
                 })
             })
         })
